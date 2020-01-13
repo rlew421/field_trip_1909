c135fe83f6608769237f4997ed9e5be6d0c4e2ef
Rachel Lew <rlew07@gmail.com>
2020-01-13 11:23:42 -0700

    Add data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+southwest = Airline.create(name: "Southwest")
+southwest_1 = southwest.flights.create(number: "SW1", date: "10/10/20", time: "1300", departure_city: "Minneapolis", arrival_city: "Nashville")
+southwest_2 = southwest.flights.create(number: "SW2", date: "10/11/20", time: "1500", departure_city: "Nashville", arrival_city: "Minneapolis")
+mike = Passenger.create!(name: "Mike Dao", age: 30)
